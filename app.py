b4b3bc80b422c1f4aa176901fabf5a40b7c93826
unknown <marinmainka@gmail.com>
2025-05-13 21:10:57 +0200

    it works

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from src.map_modificator import add_click_marker
 from src.map_generator import generate_marine_map
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def show_map(request: Request):
-    # Créer une carte centrée sur Concarneau
     m = generate_marine_map(location=[47.8722, -3.9216], map_name="sea_map")
     add_click_marker(m)
     
-    # Sauvegarder dans un fichier temporaire HTML (embeddable)
     map_html = m.get_root().render()
 
     return templates.TemplateResponse("map.html", {
