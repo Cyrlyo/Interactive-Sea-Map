0df87713ce817e0c27bf950e798ecdd88b4a2285
unknown <marinmainka@gmail.com>
2025-05-13 21:45:47 +0200

    custom marker

--- src/map_modificator.py
+++ src/map_modificator.py
@@ -9,41 +9,49 @@ def add_click_marker(sea_map: Map) -> None:
 def custom_click_marker(sea_map: Map) -> None:
     template = Template(
         """
-            {% macro script(this, kwargs) %}
-                var markerCount = 0;  // compteur de marqueurs
+        {% macro script(this, kwargs) %}
+            var markerCount = 0;
 
-                function newMarker(e) {
-                    markerCount += 1;
+            function newMarker(e) {
+                markerCount += 1;
 
-                    var new_mark = L.marker().setLatLng(e.latlng).addTo({{ this._parent.get_name() }});
-                    new_mark.dragging.enable();
+                var new_mark = L.marker().setLatLng(e.latlng, { draggable: true }).addTo({{ this._parent.get_name() }});
+                new_mark.dragging.enable();
 
-                    // Supprimer le marqueur au double clic
-                    new_mark.on('dblclick', function(e) {
-                        {{ this._parent.get_name() }}.removeLayer(e.target);
-                        markerCount -= 1;
-                        updateMarkerCounter(); // met à jour le compteur après suppression
-                    });
+                // Display coordinates in the popup
+                function updatePopup(marker) {
+                    var lat = marker.getLatLng().lat.toFixed(4);
+                    var lng = marker.getLatLng().lng.toFixed(4);
+                    marker.bindPopup("Marker: " + markerCount + "<br>Lat: " + lat + "<br>Lng: " + lng).openPopup();
+                }
 
-                    var lat = e.latlng.lat.toFixed(4),
-                        lng = e.latlng.lng.toFixed(4);
+                updatePopup(new_mark); // afficher dès l'ajout
 
-                    new_mark.bindPopup("Marker: " + markerCount + "<br>Lat: " + lat + "<br>Lng: " + lng);
+                // Update the pupop when the marker moves
+                new_mark.on('dragend', function(e) {
+                    updatePopup(e.target);
+                });
 
+                // Delete when double click
+                new_mark.on('dblclick', function(e) {
+                    {{ this._parent.get_name() }}.removeLayer(e.target);
+                    markerCount -= 1;
                     updateMarkerCounter();
-                }
+                });
 
-                // Met à jour un compteur visible dans la page
-                function updateMarkerCounter() {
-                    var counter = document.getElementById("marker-counter");
-                    if (counter) {
-                        counter.innerText = "Nombre de marqueurs : " + markerCount;
-                    }
-                }
+                updateMarkerCounter();
+            }
 
-                {{ this._parent.get_name() }}.on('click', newMarker);
-            {% endmacro %}
-            """
+            function updateMarkerCounter() {
+                var counter = document.getElementById("marker-counter");
+                if (counter) {
+                    counter.innerText = "Number of markers : " + markerCount;
+                }
+            }
+
+            {{ this._parent.get_name() }}.on('click', newMarker);
+        {% endmacro %}
+        """
     )
     
     click_marker = folium.ClickForMarker("<b>Lat:</b> ${lat}<br /><b>Lon:</b> ${lng}<b>Marker n°:</b> ${counter}")
