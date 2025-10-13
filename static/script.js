bdf936fa7b9e4c243f0a8394aeead0d49166999a
unknown <marinmainka@gmail.com>
2025-10-13 21:13:19 +0200

    some updates

--- static/script.js
+++ static/script.js
@@ -1,7 +1,3 @@
 async function importGPX() {
-    const test = "Test"
-    const header = document.getElementById('button-div-1')
-
-    header.innerHTML += `<p class="test">${test}</p>`
     console.log("It worked")
 }
